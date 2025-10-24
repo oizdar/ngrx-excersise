df6c14ad8241ff87e42c4b1fd8dde3102aa088a7
Radosław Malinowski <malinowski.rad@gmail.com>
2025-10-24 08:08:27 +0200

    action with properties

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -4,8 +4,8 @@ import { increment } from "./counter.actions";
 const initialState = 0;
 export const counterReducer = createReducer(
   initialState,
-  on(increment, state => state + 1)
-)
+  on(increment, (state, action) => state + action.value)
+);
 
 // export function counterReducer(state = initialState) {
 //   return state;

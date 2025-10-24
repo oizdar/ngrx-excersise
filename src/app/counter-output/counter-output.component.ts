43e01c5699a487e698ef50d77d77f5e0e362bcfb
Radosław Malinowski <malinowski.rad@gmail.com>
2025-10-24 08:03:37 +0200

    handle first action

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
-import { CounterService } from '../counter.service';
 import { Store } from "@ngrx/store";
 import { AsyncPipe } from "@angular/common";
 
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,8 +1,10 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
+import { increment } from "./counter.actions";
 
 const initialState = 0;
 export const counterReducer = createReducer(
-  initialState
+  initialState,
+  on(increment, state => state + 1)
 )
 
 // export function counterReducer(state = initialState) {

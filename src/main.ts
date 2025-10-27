7bcc9c5c6365dc30b858bc311215fd8edd2f8549
Radosław Malinowski <malinowski.rad@gmail.com>
2025-10-27 09:23:37 +0100

    add ngrx/effects pkg

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from "./app/store/counter.reducer";
+import { provideEffects } from '@ngrx/effects';
 
 bootstrapApplication(AppComponent, {
     providers: [
-      provideStore({
+    provideStore({
         counter: counterReducer
-      })
-    ]
+    }),
+    provideEffects()
+]
 });

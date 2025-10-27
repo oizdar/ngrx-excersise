67912b68aae55ce44d4e04cff7d45364f31f9296
Radosław Malinowski <malinowski.rad@gmail.com>
2025-10-27 09:38:35 +0100

    using store data in effect

--- src/app/store/counter.effects.ts
+++ src/app/store/counter.effects.ts
@@ -1,18 +1,22 @@
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { decrement, increment } from "./counter.actions";
-import { tap } from "rxjs";
+import { tap, withLatestFrom } from "rxjs";
 import { Injectable } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { selectCount } from "./counter.selectors";
 
 @Injectable()
 export class CounterEffects {
   saveCount = createEffect(() => this.actions$.pipe(
     ofType(increment, decrement),
-    tap((action) => {
+    withLatestFrom(this.store.select(selectCount)),
+    tap(([action, selectCount]) => {
       console.log(action);
-      localStorage.setItem('count', action.value.toString());
+      console.log(selectCount);
+      localStorage.setItem('count', selectCount.toString());
     })
   ), { dispatch: false });
 
-  constructor(private actions$: Actions) {
+  constructor(private actions$: Actions, private store: Store<{counter: number}>) {
   }
 }
